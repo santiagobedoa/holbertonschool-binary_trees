6ab335ee9555d55a6b6bc61a3b3deca283aa1a0a
Santiago Bedoya <santiagobedoa@gmail.com>
2022-07-26 12:52:52 -0500

    Tue Jul 26 12:52:52 -05 2022

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to a node to check
+ *
+ * Return: 1 if the node is a leaf, 0 otherwise.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node != NULL && node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+	return (0);
+}
