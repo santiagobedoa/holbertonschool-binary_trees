a926749ca7faa160278da8e869774ad04e9072d9
Santiago Bedoya <santiagobedoa@gmail.com>
2022-07-26 12:13:51 -0500

    Tue Jul 26 12:13:51 -05 2022

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,24 +12,24 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node = NULL;
 
-        if (!parent)
-        {
-                return (NULL);
-        }
-        new_node = malloc(sizeof(binary_tree_t));
-        if (!new_node)
-        {
-                return (NULL);
-        }
-        new_node->n = value;
-        new_node->parent = parent;
-        if (parent->left)
-        {
-                new_node->left = parent->left;
-                new_node->left->parent = new_node;
-        }
-        parent->left = new_node;
-        return (new_node);
+	if (!parent)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+	{
+		return (NULL);
+	}
+	new_node->n = value;
+	new_node->parent = parent;
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
+	}
+	parent->left = new_node;
+	return (new_node);
 }
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in the new node
+ *
+ * Description: If parent already has a right-child, the new node must take its
+ * place, and the old right-child must be set as the right-child of the new
+ * node.
+ *
+ * Return: pointer to the created node, or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	if (!parent)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+	{
+		return (NULL);
+	}
+	new_node->n = value;
+	new_node->parent = parent;
+	if (parent->right)
+	{
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
+	}
+	parent->right = new_node;
+	return (new_node);
+}
