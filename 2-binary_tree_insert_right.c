ebde12516e60e91956ea3596c9df4f655a0651ff
Santiago Bedoya <santiagobedoa@gmail.com>
2022-07-27 12:30:30 -0500

    Wed Jul 27 12:30:30 -05 2022

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = calloc(1, sizeof(binary_tree_t));
 	if (!new_node)
 	{
 		return (NULL);
